a4215fc63edea7e26f0b5311130df3ce2de69ce7
Ryar Nyah <ryarnyah@gmail.com>
2020-09-05 23:52:41 +0200

    Add go-plugin sink to permit external integration

--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.12
 
 require (
 	github.com/Shopify/sarama v1.27.0
+	github.com/golang/protobuf v1.4.1
+	github.com/hashicorp/go-plugin v0.0.0-20181030172320-54b6ff97d818
 	github.com/influxdata/influxdb v1.8.2
 	github.com/influxdata/platform v0.0.0-20190117200541-d500d3cf5589 // indirect
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983 // indirect
 	github.com/olivere/elastic v6.2.35+incompatible
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
 	github.com/sirupsen/logrus v1.6.0
+	google.golang.org/grpc v1.33.0-dev
+	google.golang.org/protobuf v1.25.0
 )
---
+++ pkg/sinks/plugin/shared/interface.go
@@ -0,0 +1,43 @@
+package shared
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/ryarnyah/kafka-offset/pkg/sinks/plugin/proto"
+	"google.golang.org/grpc"
+)
+
+// Handshake is a common handshake that is shared by plugin and host.
+var Handshake = plugin.HandshakeConfig{
+	// This isn't required when using VersionedPlugins
+	ProtocolVersion:  1,
+	MagicCookieKey:   "KAFKA_OFFSET_PLUGIN",
+	MagicCookieValue: "kafka-offset",
+}
+
+// PluginMap is the map of plugins we can dispense.
+var PluginMap = map[string]plugin.Plugin{
+	"kafka_grpc": &KafkaGRPCPlugin{},
+}
+
+type KafkaPlugin interface {
+	WriteKafkaMetrics([]interface{}) error
+}
+
+type KafkaGRPCPlugin struct {
+	// GRPCPlugin must still implement the Plugin interface
+	plugin.Plugin
+	// Concrete implementation, written in Go. This is only used for plugins
+	// that are written in Go.
+	Impl KafkaPlugin
+}
+
+func (p *KafkaGRPCPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
+	proto.RegisterKafkaPluginService(s, proto.NewKafkaPluginService(&GRPCServer{Impl: p.Impl}))
+	return nil
+}
+
+func (p *KafkaGRPCPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
+	return &GRPCClient{client: proto.NewKafkaPluginClient(c)}, nil
+}
